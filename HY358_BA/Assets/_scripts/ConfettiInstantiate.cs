65b29725b6b74ab13cb24adaabec2b88b0737320
XristinaAC <christina-v7@hotmail.com>
2021-01-24 22:38:22 +0200

    Game Files

---
+++ HY358_BA/Assets/_scripts/ConfettiInstantiate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfettiInstantiate : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject ConfettiPrefab;
+    public int confettiNum = 20;
+    public Transform crownMiddle;
+    public float radius = 5.0F;
+    public float power = 10.0F;
+    public Material mat;
+    public Color conColor;
+
+    private Rigidbody[] confetti;
+
+
+    // Update is called once per frame
+    public void ConfettiExplosion()
+    {
+        confetti = new Rigidbody[confettiNum];
+        Rigidbody rb = ConfettiPrefab.GetComponent<Rigidbody>();
+        for (int i = 0; i < confettiNum; ++i)
+        {
+            confetti[i] = Instantiate(rb, crownMiddle.position, Quaternion.identity) as Rigidbody;
+            confetti[i].AddForce(crownMiddle.position,ForceMode.Impulse);
+            mat.color = conColor;
+        }
+    }
+}
